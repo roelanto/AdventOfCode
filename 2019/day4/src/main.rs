4766b15c72d13e0c7d1168ef09d7079c6010d66b
Roelant Ossewaarde <roelanto@gmail.com>
2019-12-04 15:02:44 +0100

    second question; refactor of the first.

--- 2019/day4/src/main.rs
+++ 2019/day4/src/main.rs
@@ -1,5 +1,3 @@
-
-
 fn rle_reduce_int(num : &str) -> Vec<u32>{
     let digits: Vec<_> = num.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
     return rle_reduce(digits);
@@ -8,19 +6,23 @@ fn rle_reduce_int(num : &str) -> Vec<u32>{
 fn rle_reduce(v:Vec<u32>) -> Vec<u32> {
     let mut prev_read = v[0]+1;
     let mut vnew:Vec<_> = Vec::new();
+    let mut counter = 0;
+    let mut runcounter = 0;
     for val in v.into_iter() {
+	counter = counter + 1;
 	if val != prev_read {
-	    vnew.push(val);
-	}
+	    if runcounter > 0 {
+		vnew.push(counter);
+	    }
+	    counter = 0;
+	} 
 	prev_read = val;
+	runcounter = runcounter + 1;
     }
+    vnew.push(counter+1);
     return vnew;
 }
 
-
-//fn join(a: &[i32]) -> String {
-//    a.iter().fold(String::new(),|mut s,&n| {write!(s,"{}",n).ok(); s})
-//}
 fn determine_order(st : &str) -> bool {
     let m1 = st.chars().map(|x| x.to_digit(10).unwrap())  ;
     let m1vec = m1.clone().collect::<Vec<u32>>();
@@ -29,20 +31,26 @@ fn determine_order(st : &str) -> bool {
     m2vec==m1vec
 }
 
+fn has_multiples (v : Vec<u32>) -> u32 {
+    v.iter().map(|x| if *x > 1 {1} else {0}).sum()
+}
+
+fn has_doubles (v : Vec<u32>) -> u32 {
+    v.iter().map(|x| if *x == 2 {1} else {0}).sum()
+}
+
 fn main() {
+    
     let v:Vec<_> = (240298..784956).map(|x| x).collect();
-    //    let v = vec![1,2,3,4,5,6,7,8,9,11,10];
-//    let v = vec![111111, 223450, 123789];
+
     let viter = v.iter();
     // determine which have double input
-    let vfilter:Vec<&u32> = viter.filter(|x| (x.to_string()).len() != (rle_reduce_int(&x.to_string())).len()).collect();
+
+    let vfilter:Vec<&u32> = viter.filter(|x| has_multiples(rle_reduce_int(&x.to_string())) > 0).collect();
     println!("filter1: {:?}", vfilter.len());
     let vfilter2:Vec<&&u32> = vfilter.iter().filter(|x| determine_order(&x.to_string())).collect::<Vec<&&u32>>();
-    println!("filter2: {:?}", vfilter2.len());
+    println!("filter2: {:?} is the answer to the first question.", vfilter2.len());
 
-    println!("rle: {:?}", rle_reduce(vec![1,2,3,3,2,1]));
-    println!("rle: {:?}", rle_reduce(vec![0]));
-    let num = "123321";
-    println!("rle: {:?}", rle_reduce_int(num));
-
+    let vfilter3:Vec<&&&u32> = vfilter2.iter().filter(|x| has_doubles(rle_reduce_int(&x.to_string())) > 0).collect();
+    println!("filter3: {:?} is the answer to the second question.", vfilter3.len());
 }
